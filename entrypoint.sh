e4a7abe1512e4751d974aa7c67ce65341fa5b712
Danevi01 <dgeuking@gmail.com>
2025-06-20 19:59:59 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,23 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $(NF-2);exit}'`
 
+## Add branch logic based on the BRANCH environment variable
+BRANCH_FLAG=""
+if [ "${BRANCH}" != "release" ]; then
+    BRANCH_FLAG="-beta ${BRANCH}"
+fi
 
 ## if auto_update is not set or to 1 update
 if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
-	./steamcmd/steamcmd.sh +force_install_dir /home/container +login anonymous +app_update 258550 +quit
+    echo "Starting SteamCMD update for branch: ${BRANCH}..."
+	./steamcmd/steamcmd.sh +force_install_dir /home/container +login anonymous +app_update 258550 ${BRANCH_FLAG} validate +quit
+    if [ $? -ne 0 ]; then
+        echo "ERROR: SteamCMD update failed for branch ${BRANCH}!"
+        exit 1
+    fi
+    echo "SteamCMD update finished for branch: ${BRANCH}."
 else
-    echo -e "Not updating game server as auto update was set to 0. Starting Server"
+    echo -e "Not updating game server as auto update was set to 0. Starting Server"
 fi
 
 # Replace Startup Variables
@@ -17,27 +28,28 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 if [[ "${FRAMEWORK}" == "carbon" ]]; then
-    # Carbon: https://github.com/CarbonCommunity/Carbon.Core
-    echo "Updating Carbon..."
-    curl -sSL "https://github.com/CarbonCommunity/Carbon.Core/releases/download/production_build/Carbon.Linux.Release.tar.gz" | tar zx
-    echo "Done updating Carbon!"
+    # Carbon: https://github.com/CarbonCommunity/Carbon.Core
+    echo "Updating Carbon..."
+    curl -sSL "https://github.com/CarbonCommunity/Carbon.Core/releases/download/production_build/Carbon.Linux.Release.tar.gz" | tar zx
+    echo "Done updating Carbon!"
 
-    export DOORSTOP_ENABLED=1
-    export DOORSTOP_TARGET_ASSEMBLY="$(pwd)/carbon/managed/Carbon.Preloader.dll"
-    MODIFIED_STARTUP="LD_PRELOAD=$(pwd)/libdoorstop.so ${MODIFIED_STARTUP}"
+    export DOORSTOP_ENABLED=1
+    export DOORSTOP_TARGET_ASSEMBLY="$(pwd)/carbon/managed/Carbon.Preloader.dll"
+    MODIFIED_STARTUP="LD_PRELOAD=$(pwd)/libdoorstop.so ${MODIFIED_STARTUP}"
 
 elif [[ "$OXIDE" == "1" ]] || [[ "${FRAMEWORK}" == "oxide" ]]; then
-    # Oxide: https://github.com/OxideMod/Oxide.Rust
-    echo "Updating uMod..."
-    curl -sSL "https://github.com/OxideMod/Oxide.Rust/releases/latest/download/Oxide.Rust-linux.zip" > umod.zip
-    unzip -o -q umod.zip
-    rm umod.zip
-    echo "Done updating uMod!"
+    # Oxide: https://github.com/OxideMod/Oxide.Rust
+    echo "Updating uMod..."
+    curl -sSL "https://github.com/OxideMod/Oxide.Rust/releases/latest/download/Oxide.Rust-linux.zip" > umod.zip
+    unzip -o -q umod.zip
+    rm umod.zip
+    echo "Done updating uMod!"
 # else Vanilla, do nothing
 fi
 
 # Fix for Rust not starting
 export LD_LIBRARY_PATH=$(pwd)/RustDedicated_Data/Plugins/x86_64:$(pwd)
 
-# Run the Server
+# Run the Server using the wrapper
+echo "Starting Rust server via wrapper..."
 node /wrapper.js "${MODIFIED_STARTUP}"
